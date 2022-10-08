857b400408b66d7a7e6267ef156ec1cc77b01270
Alan Neres <alanneres10@hotmail.com>
2022-10-08 02:54:49 -0300

    chore: adjust next config and ajust page generate in plop

--- next.config.js
+++ next.config.js
@@ -1,18 +1,11 @@
 /** @type {import('next').NextConfig} */
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = require('next-pwa');
-const isProd = process.env.NODE_ENV === 'production';
 
-const nextConfig = withPWA({
+const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   compiler: {
     styledComponents: true
-  },
-  pwa: {
-    dest: 'public',
-    disable: !isProd
   }
-});
+};
 
 module.exports = nextConfig;
