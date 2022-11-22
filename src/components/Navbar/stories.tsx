a0b557a9b74b7c2f50b376f1be3499cbc59daf5f
Alan Neres <alanneres10@hotmail.com>
2022-11-22 01:06:04 -0300

    fix: navbar-storie

--- src/components/Navbar/stories.tsx
+++ src/components/Navbar/stories.tsx
@@ -7,6 +7,4 @@ export default {
   component: Navbar
 } as ComponentMeta<typeof Navbar>;
 
-export const Basic: ComponentStory<typeof Navbar> = (args) => (
-  <Navbar {...args} />
-);
+export const Basic: ComponentStory<typeof Navbar> = () => <Navbar />;
