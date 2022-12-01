34cbb47032fc9f98e788d2118e1a62eaf9adc43b
Alan Neres <alanneres10@hotmail.com>
2022-12-01 20:36:01 -0300

    fix: data-formatter test

--- src/utils/helpers/format-date/test.ts
+++ src/utils/helpers/format-date/test.ts
@@ -4,9 +4,7 @@ const value = '2021-03-26T11:51:15';
 
 describe('function dateFormater()', () => {
   it('should format date correctly', () => {
-    expect(dateFormatter({ date: value })).toMatchInlineSnapshot(
-      `"26/03/2021"`
-    );
+    expect(dateFormatter({ date: value })).toBe(`26/03/2021`);
   });
 
   it('should format date when customOptions is passed', () => {
@@ -22,7 +20,7 @@ describe('function dateFormater()', () => {
           second: 'numeric'
         }
       })
-    ).toMatchInlineSnapshot(`"26/03/2021 11:51:15"`);
+    ).toBe(`26/03/2021 11:51:15`);
   });
 
   it('should return undefined when undefined is passed', () => {
