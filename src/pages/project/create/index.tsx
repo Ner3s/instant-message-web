ef3f316a20b1231db4fb891f9be513995588dc31
Alan Neres <alanneres10@hotmail.com>
2023-04-24 01:05:35 -0300

    fix: add props in createProject page

--- src/pages/project/create/index.tsx
+++ src/pages/project/create/index.tsx
@@ -2,10 +2,20 @@ import { Base } from '@/components/Base';
 
 import { CreateProjectTemplate } from '@/templates/CreateProject';
 
+import { useAuth } from '@/contexts/use-auth';
+
+import { useUploadFile } from '@/hooks/use-upload-file';
+
 export default function CreateProject() {
+  const { handleUploadFile } = useUploadFile();
+  const { user } = useAuth();
+
   return (
     <Base>
-      <CreateProjectTemplate />
+      <CreateProjectTemplate
+        owner_id={user.uid}
+        handleUploadFile={handleUploadFile}
+      />
     </Base>
   );
 }
