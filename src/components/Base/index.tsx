8be1da87c2648d4244a417d3bf157af9003bab42
Alan Neres <alanneres10@hotmail.com>
2023-05-13 00:39:34 -0300

    chore: add spinner for loading page

--- src/components/Base/index.tsx
+++ src/components/Base/index.tsx
@@ -2,6 +2,10 @@ import { ReactNode } from 'react';
 
 import { Navbar } from '@/components/Navbar';
 
+import { Spinner } from '../Spinner';
+
+import { useAuth } from '@/contexts/use-auth';
+
 import { MENU_LINKS } from '@/utils/constants/menu-links';
 
 import * as S from './styles';
@@ -11,9 +15,18 @@ export interface BaseProps {
 }
 
 function Base({ children }: BaseProps) {
+  const { isLoading } = useAuth();
   return (
     <S.Container>
-      <S.Main>{children}</S.Main>
+      <S.Main>
+        {isLoading ? (
+          <S.WrapperSpinner>
+            <Spinner size="14rem" color="#fff" />
+          </S.WrapperSpinner>
+        ) : (
+          children
+        )}
+      </S.Main>
       <Navbar menuLinks={MENU_LINKS} />
     </S.Container>
   );
